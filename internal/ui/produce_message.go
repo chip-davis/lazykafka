91a3f33b94f1e240f51f80213a847160db188d00
chip-davis <chipydavis99@gmail.com>
2026-01-26 09:01:47 -0600

    feat: added ui for produce_message (untested)

---
+++ internal/ui/produce_message.go
@@ -0,0 +1,177 @@
+package ui
+
+import (
+	"github.com/charmbracelet/bubbles/textinput"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+var placeholders = []string{
+	"Partition Number",
+	"Key Serde",
+	"Value Serde",
+	"Key",
+	"Value",
+	"Headers",
+}
+
+type ProduceMessageValues struct {
+	PartitionNumber string
+	KeySerde        string
+	ValueSerde      string
+	Key             string
+	Value           string
+	Headers         string
+}
+
+type formInput struct {
+	field string
+	input textinput.Model
+}
+
+type ProduceMessageForm struct {
+	width     int
+	height    int
+	topicName string
+	focused   int
+	inputs    []formInput
+}
+
+type ProduceMsg struct {
+	TopicName       string
+	PartitionNumber string
+	KeySerde        string
+	ValueSerde      string
+	Key             string
+	Value           string
+	Headers         string
+}
+
+func NewProduceMessageForm(topicName string) ProduceMessageForm {
+
+	fields := []string{
+		"PartitionNumber",
+		"KeySerde",
+		"ValueSerde",
+		"Key",
+		"Value",
+		"Headers",
+	}
+
+	placeholders := []string{
+		"Partition Number",
+		"Key Serde",
+		"Value Serde",
+		"Key",
+		"Value",
+		"Headers",
+	}
+
+	inputs := make([]formInput, len(fields))
+	for i := range fields {
+		ti := textinput.New()
+		ti.Placeholder = placeholders[i]
+		if placeholders[i] != "Value" {
+			ti.CharLimit = 100
+		}
+		inputs[i] = formInput{
+			field: fields[i],
+			input: ti,
+		}
+	}
+
+	inputs[0].input.Focus()
+
+	return ProduceMessageForm{
+		topicName: topicName,
+		inputs:    inputs,
+		focused:   0,
+	}
+}
+
+func (f ProduceMessageForm) Init() tea.Cmd { return textinput.Blink }
+func (f ProduceMessageForm) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "tab":
+			f.inputs[f.focused].input.Blur()
+			f.focused = (f.focused + 1) % len(f.inputs)
+
+			cmd = f.inputs[f.focused].input.Focus()
+			return f, cmd
+		case "shift+tab":
+			f.inputs[f.focused].input.Blur()
+			f.focused = (f.focused - 1 + len(f.inputs)) % len(f.inputs)
+			cmd = f.inputs[f.focused].input.Focus()
+			return f, cmd
+		case "enter":
+			values := ProduceMessageValues{}
+
+			for _, fi := range f.inputs {
+				switch fi.field {
+				case "PartitionNumber":
+					values.PartitionNumber = fi.input.Value()
+				case "KeySerde":
+					values.KeySerde = fi.input.Value()
+				case "ValueSerde":
+					values.ValueSerde = fi.input.Value()
+				case "Key":
+					values.Key = fi.input.Value()
+				case "Value":
+					values.Value = fi.input.Value()
+				case "Headers":
+					values.Headers = fi.input.Value()
+				}
+			}
+
+			return f, func() tea.Msg {
+				return ProduceMsg{
+					TopicName:       f.topicName,
+					PartitionNumber: values.PartitionNumber,
+					KeySerde:        values.KeySerde,
+					ValueSerde:      values.ValueSerde,
+					Key:             values.Key,
+					Value:           values.Value,
+					Headers:         values.Headers,
+				}
+			}
+		case "esc":
+			return f, nil
+		}
+	}
+	ti, cmd := f.inputs[f.focused].input.Update(msg)
+	f.inputs[f.focused].input = ti
+	return f, cmd
+}
+
+func (f ProduceMessageForm) View() string {
+	title := FormTitleStyle.Render("Produce new message")
+
+	var renderedInputs []string
+	for _, fi := range f.inputs {
+		input := fi.input.View()
+		renderedInputs = append(renderedInputs, input)
+	}
+
+	help := FormHelpStyle.Render(
+		"enter: produce message • esc: cancel • tab: switch focus",
+	)
+
+	content := lipgloss.JoinVertical(
+		lipgloss.Left,
+		append(
+			[]string{title},
+			append(renderedInputs, help)...,
+		)...,
+	)
+
+	return lipgloss.Place(
+		f.width,
+		f.height,
+		lipgloss.Center,
+		lipgloss.Center,
+		content,
+	)
+}
